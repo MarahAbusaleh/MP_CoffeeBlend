262e687dd57f9185981a2e69cd3794a1046d460e
MarahAbusaleh <marah.abusaleh12@gmail.com>
2023-09-23 21:35:46 +0300

    mmmm

--- resources/views/Admin/Layouts/navbar.blade.php
+++ resources/views/Admin/Layouts/navbar.blade.php
@@ -5,14 +5,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Coffee Blend - @yield('title')</title>
-    <link rel="shortcut icon" type="image/png" href="../assets/images/logos/SClogo.png" />
-    <link rel="stylesheet" href="../assets/css/styles.min.css" />
+    <link rel="shortcut icon" type="image/png" href="{{ asset('../assets/images/logos/SClogo.png') }}" />
+    <link rel="stylesheet" href="{{ asset('../assets/css/styles.min.css') }}" />
     <link ret="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="//cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-
-
 </head>
 
 <body>
@@ -145,7 +143,7 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2" data-bs-toggle="dropdown"
                     aria-expanded="false">
-                    <img src="../assets/images/profile/MyProfailPicture.jpeg" alt="" width="35" height="35" class="rounded-circle">
+                    <img src="{{ asset('../assets/images/profile/MyProfailPicture.jpeg') }}" alt="" width="35" height="35" class="rounded-circle">
                     </a>
                     <div class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop2">
                     <div class="message-body">
