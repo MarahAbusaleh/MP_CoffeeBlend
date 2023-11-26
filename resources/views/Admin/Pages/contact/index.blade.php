008e525987fd18642aafac0b444b19f8e419d2ce
MarahAbusaleh <marah.abusaleh12@gmail.com>
2023-11-26 22:41:41 +0300

    Marah Update 26/11

--- resources/views/Admin/Pages/contact/index.blade.php
+++ resources/views/Admin/Pages/contact/index.blade.php
@@ -7,13 +7,7 @@
 
         <!--  Row 1 -->
         <div class="row">
-            <h2>Discount Codes</h2>
-            <center>
-                <div class="col-lg-2">
-                    <a href="{{ route('discount.create') }}" class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2"><i
-                            class="ti ti-plus"></i> Add Discount Code</a>
-                </div>
-            </center>
+            <h2>Contact Message</h2>
         </div>
         <!--  Row 2 -->
         <div class="row">
